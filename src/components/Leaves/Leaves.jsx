f36c106c1cc57b43e10696048f110613b1ca4c43
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-07-02 21:55:00 +0530

    leaves ui updated

--- src/components/Leaves/Leaves.jsx
+++ src/components/Leaves/Leaves.jsx
@@ -14,6 +14,8 @@ const Leaves = () => {
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
+  const [loading, setLoading] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setLeaveData({
@@ -33,6 +35,8 @@ const Leaves = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    handleLoading(true);
+    setLoading(true);
     if (
       !leaveData.leaveType ||
       !leaveData.reason ||
@@ -53,12 +57,12 @@ const Leaves = () => {
     const leaveDataWithDays = {
       ...leaveData,
       numberOfDays,
-      };
-      console.log(leaveDataWithDays);
+    };
+    console.log(leaveDataWithDays);
 
     setError(""); // Clear any previous error messages
 
-      const url = config.LEAVE_SUBMIT_URL;
+    const url = config.LEAVE_SUBMIT_URL;
     fetch(url, {
       method: "POST",
       headers: {
@@ -79,82 +83,122 @@ const Leaves = () => {
           toDate: "",
           email: "",
         });
-        setTimeout(() => setSuccessMessage(""), 3000); // Clear message after 3 seconds
+
+        setLoading(false);
+
+        handleLoading(false);
+        setTimeout(() => setSuccessMessage(""), 4000); // Clear message after 3 seconds
       })
       .catch((error) => {
         console.error("Error sending data to Google Apps Script:", error);
         setError("Error submitting leave request.");
       });
   };
+  const handleLoading = (load) => {
+    load == true
+      ? (document.getElementById("root").style.opacity = "0.8")
+      : (document.getElementById("root").style.opacity = "1");
+  };
 
   return (
-     <div>
+    <div>
+      <div
+        aria-label="Orange and tan hamster running in a metal wheel"
+        role="img"
+        class="wheel-and-hamster"
+        style={{
+          position: "absolute",
+          display: loading ? "block" : "none",
+          top: "42%",
+          left: "45%",
+          zIndex: "100",
+        }}
+      >
+        <div class="wheel"></div>
+        <div class="hamster">
+          <div class="hamster__body">
+            <div class="hamster__head">
+              <div class="hamster__ear"></div>
+              <div class="hamster__eye"></div>
+              <div class="hamster__nose"></div>
+            </div>
+            <div class="hamster__limb hamster__limb--fr"></div>
+            <div class="hamster__limb hamster__limb--fl"></div>
+            <div class="hamster__limb hamster__limb--br"></div>
+            <div class="hamster__limb hamster__limb--bl"></div>
+            <div class="hamster__tail"></div>
+          </div>
+        </div>
+        <div class="spoke"></div>
+      </div>
       <h1 style={{ textAlign: "center" }}>Leave Application Form</h1>
-      <p style={{ textAlign: "center" }}>  Make Sure to check the leave balance before applying</p>
-    <form onSubmit={handleSubmit}>
-      {successMessage && <h1 style={{ color: "green" }}>{successMessage}</h1>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      <p style={{ textAlign: "center" }}>
+        {" "}
+        Make Sure to check the leave balance before applying
+      </p>
+      <form onSubmit={handleSubmit}>
+        {successMessage && <h1 style={{ color: "green" }}>{successMessage}</h1>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <div>
-        <label>Leave Type:</label>
-        <select
-          name="leaveType"
-          value={leaveData.leaveType}
-          onChange={handleChange}
-          required
-        >
-          <option value="">--Select Leave Type--</option>
-          <option value="bereavement">Bereavement</option>
-          <option value="casual">Casual</option>
-          <option value="wellness">Wellness</option>
-          <option value="wedding">Wedding</option>
-        </select>
-      </div>
+        <div>
+          <div>
+            <label>Employee Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={leaveData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <label>Leave Type:</label>
+          <select
+            name="leaveType"
+            value={leaveData.leaveType}
+            onChange={handleChange}
+            required
+          >
+            <option value="">--Select Leave Type--</option>
+            <option value="bereavement">Bereavement</option>
+            <option value="casual">Casual</option>
+            <option value="wellness">Wellness</option>
+            <option value="wedding">Wedding</option>
+          </select>
+        </div>
 
-      <div>
-        <label>Reason for Leave:</label>
-        <textarea
-          name="reason"
-          value={leaveData.reason}
-          onChange={handleChange}
-          required
-        />
-      </div>
+        <div>
+          <label>Reason for Leave:</label>
+          <textarea
+            name="reason"
+            value={leaveData.reason}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-      <div>
-        <label>From Date:</label>
-        <input
-          type="date"
-          name="fromDate"
-          value={leaveData.fromDate}
-          onChange={handleChange}
-          required
-        />
-      </div>
+        <div>
+          <label>From Date:</label>
+          <input
+            type="date"
+            name="fromDate"
+            value={leaveData.fromDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-      <div>
-        <label>To Date:</label>
-        <input
-          type="date"
-          name="toDate"
-          value={leaveData.toDate}
-          onChange={handleChange}
-          required
-        />
-      </div>
+        <div>
+          <label>To Date:</label>
+          <input
+            type="date"
+            name="toDate"
+            value={leaveData.toDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-      <div>
-        <label>Employee Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={leaveData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-
-      <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );

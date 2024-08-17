4b5ed3dd32199d23a0c853684cdd8ff1d8a51d2a
ujala <ujalasaini355@gmail.com>
2024-08-17 18:48:13 +0530

    change the form URL

--- public/api.js
+++ public/api.js
@@ -1,8 +1,7 @@
 // config.js
 const config = {
   FORM_SUBMIT_URL:
-    // "https://script.google.com/macros/s/AKfycbzPUKngngA4Wp35fHNUBOHaNQ7JmUQSTtzXnYzHfKQ8U7Bsg52IPoMaA7rvhK6MNHMe8A/exec",
-    "https://script.google.com/macros/s/AKfycbz0jA6Wj2WU2qEheLL9qcigpSAx26axJvlLF7P-ILG_evgcBZwra6u0cTNWs6-PrWTnYw/exec",
+    "https://script.google.com/macros/s/AKfycbzPUKngngA4Wp35fHNUBOHaNQ7JmUQSTtzXnYzHfKQ8U7Bsg52IPoMaA7rvhK6MNHMe8A/exec",
   LEAVE_SUBMIT_URL:
     "https://script.google.com/macros/s/AKfycbzPUKngngA4Wp35fHNUBOHaNQ7JmUQSTtzXnYzHfKQ8U7Bsg52IPoMaA7rvhK6MNHMe8A/exec",
 };

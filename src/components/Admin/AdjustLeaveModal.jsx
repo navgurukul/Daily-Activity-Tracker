7c2c3676fcbc053ef4b0499797d8168b686f5947
komalahire <komala@navgurukul.org>
2025-07-05 14:05:41 +0530

    new brach deploy

--- src/components/Admin/AdjustLeaveModal.jsx
+++ src/components/Admin/AdjustLeaveModal.jsx
@@ -39,7 +39,6 @@ const AdjustLeaveModal = ({
   const [loading, setLoading] = useState(false);
   const [submitError, setSubmitError] = useState("");
 
-  
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   const leaveTypes = [
